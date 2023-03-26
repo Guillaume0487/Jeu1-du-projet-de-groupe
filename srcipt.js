32e6e6f4f0e7f0c051811a57f79cbdb229554831
Guillaume <guillaumedhoirpro@gmail.com>
2023-03-27 01:56:27 +0200

    commit

--- srcipt.js
+++ srcipt.js
@@ -1,70 +1,49 @@
 let lstWiners = []
-//verif
+
 function verify1() {
     if (lstWiners.length > 0) {
         return;
     } if (document.getElementsByClassName("card").length > 1) {
-        cacherBtn(1)
         play1();
-    } else {
-        return;
-    }
-}
-
-function verify2() {
-    if (lstWiners.length > 0) {
-        return;
-    } if (document.getElementsByClassName("card").length > 3) {
-        cacherBtn(2)
-        play2();
     } else {
         return;
     }
 }
 
 function play1() {
-    const winer = Math.ceil(Math.random() * document.getElementsByClassName("card").length - 1);
-    const playerWin = document.getElementsByClassName("card")[winer].id
-    let i = 0;
-    const Timer = setInterval(() => {
-        document.getElementsByClassName("card")[i].style.backgroundColor = "";
-        setTimeout(() => {
-            document.getElementsByClassName("card")[i].style.backgroundColor = "navy";
-        },);
-        i++;
-        if (i == document.getElementsByClassName("card").length) {
-            i = 0;
-        }
-    }, 150);
+    const tirage = setInterval(() => {
+        let select = Math.ceil(Math.random() * document.getElementsByClassName("card").length - 1);
+        blink(select);
+    }, 250);
     setTimeout(() => {
-        clearInterval(Timer);
-        for (let j = 0; j < document.getElementsByClassName("card").length; j++) {
-            document.getElementsByClassName("card")[j].style.backgroundColor = "";
-        } document.getElementsByClassName("card")[winer].className = "winer";
-        podium(playerWin);
-    }, 5000);
+        clearInterval(tirage);
+        let select = Math.ceil(Math.random() * document.getElementsByClassName("card").length - 1);
+        const winner = setInterval(() => {
+            blink(select);
+        }, 500);
+        setTimeout(() => {
+            clearInterval(winner);
+            setTimeout(() => {
+                const playerWin = document.getElementsByClassName("card")[select];
+                playerWin.className = "winer";
+                lstWiners.push(playerWin.id)
+            }, 200);
+        }, 1500);
+    }, 3000);
 }
 
-function play2() {
-    play1();
+function blink(index) {
+    document.getElementsByClassName("card")[index].style.backgroundColor = "#008080";
+    document.getElementsByClassName("card")[index].style.color = "white";
     setTimeout(() => {
-        play1();
-        setTimeout(() => {
-            play1();
-        }, 5000);
-    }, 5000);
+        document.getElementsByClassName("card")[index].style.backgroundColor = "";
+        document.getElementsByClassName("card")[index].style.color = "";
+    }, 200);
 }
 
-function podium(str) {
-    document.getElementById("lst-winers").remove();
-    let er_eme = "er"
-    if (lstWiners.length > 0) {
-        er_eme = "eme";
-    } lstWiners.push(str + " est " + (lstWiners.length + 1) + er_eme + "\n");
-    const newDiv = document.createElement("div");
-    newDiv.id = "lst-winers";
-    newDiv.innerText = lstWiners;
-    document.getElementById("players-win").appendChild(newDiv);
+function rePlay() {
+    refresh()
+    play1()
 }
 
 function refresh() {
@@ -78,22 +57,4 @@ function refresh() {
     const newDiv = document.createElement("div");
     newDiv.id = "lst-winers";
     document.getElementById("players-win").appendChild(newDiv);
-}
-
-function cacherBtn(n) {
-    for (let i = 0; i < document.getElementsByClassName("btn").length; i++) {
-        document.getElementsByClassName("btn")[i].hidden = true
-    } if (n == 1) {
-        setTimeout(() => {
-            for (let i = 0; i < document.getElementsByClassName("btn").length; i++) {
-                document.getElementsByClassName("btn")[i].hidden = false
-            }
-        }, 5000);
-    } if (n == 2) {
-        setTimeout(() => {
-            for (let i = 0; i < document.getElementsByClassName("btn").length; i++) {
-                document.getElementsByClassName("btn")[i].hidden = false
-            }
-        }, 5000 * 3);
-    }
 }
